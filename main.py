eb570c02c622a5f39a3420a622d8cda3e4ffb26d
Kyllian ROUSSELEAU <kyllianr1008@gmail.com>
2025-09-30 14:27:57 +0200

    test_com4

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello bis")
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+data = yf.download("AAPL", start="2018-01-01", end="2023-01-01")
+
+print(data)
